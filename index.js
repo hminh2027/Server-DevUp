072da0c1a01032691fc039bdf8e1fae18d6cdfd2
Minh <57801022+hminh2027@users.noreply.github.com>
2022-02-01 16:14:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
-app.use(cors())
+app.use(cors({credentials: true, origin: 'https://client-dev-up.vercel.app'}))
 app.use(cookie())
 
 // Socket
